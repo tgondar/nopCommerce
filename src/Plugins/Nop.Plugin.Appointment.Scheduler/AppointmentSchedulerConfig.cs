130bb552ad18550446b1d630000b78ba9db6013f
Telmo Gondar <telmo.gondar@devscope.net>
2020-03-03 01:06:16 +0000

    added the first ever install/unnstal, that executes a create table in SQL, probably not the best way, but for now is great.

---
+++ src/Plugins/Nop.Plugin.Appointment.Scheduler/AppointmentSchedulerConfig.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Nop.Services.Plugins;
+using Nop.Data;
+
+namespace Nop.Plugin.Appointment.Scheduler
+{
+    public class AppointmentSchedulerConfig : BasePlugin
+    {
+        private readonly IDataProvider _dataProvider;
+
+        public AppointmentSchedulerConfig(IDataProvider dataProvider)
+        {
+            _dataProvider = dataProvider;
+        }
+
+        public override void Install()
+        {
+            //create the table
+            _dataProvider.Query<object>("CREATE TABLE DBO.AAAAAAAAAA (Id INT, [Name] NVARCHAR(50))");
+
+            base.Install();
+        }
+
+        /// <summary>
+        /// Uninstall plugin
+        /// </summary>
+        public override void Uninstall()
+        {
+            _dataProvider.Query<object>("DROP TABLE DBO.AAAAAAAAAA");
+
+            base.Uninstall();
+        }
+    }
+}
